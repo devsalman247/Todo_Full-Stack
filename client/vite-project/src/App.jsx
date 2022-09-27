281f2fa085d68b739ade87a77e2fd71027ce89b3
devsalman247 <salman247.dev@gmail.com>
2022-09-27 18:55:08 +0500

    little bit updation

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -3,34 +3,43 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, addTodo] = useState(['My First Todo Task','This is second Todo task','This is third Todo Task']);
+  const todoList = todo.map(todoItem => 
+    <p className='mb-2 rounded-sm bg-slate-50 p-1 mx-3 text-base overflow-hidden'>
+      {todoItem}
+      <div>
+      <button className='bg-sky-500 text-white rounded-sm py-1 px-2 m-2 focus:outline-none'>Edit</button>
+      <button className='bg-sky-500 text-white rounded-sm p-1 m-2 focus:outline-none' onClick={deleteTask}>Delete</button>
+      </div>
+    </p>
+  );
 
-  function changeCount(){
-    setCount(count => count+1);
-  };
+  function addTask(e) {
+    const task = document.getElementById("todo_enter");
+    addTodo(todo => [...todo,task.value]);
+  }
+
+  function deleteTask(e) {
+    const task = e.currentTarget.parentElement.parentElement;
+    addTodo(todo => todo.filter(item => {
+      console.log(item);
+      console.log(task);
+      return item!==task
+    }))
+  }
 
   return (
-    <div >
+    <div className='flex items-center flex-col mt-10 h-full'>
+      <h2 className='font-medium text-2xl mb-2'>My Todo List</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} alt="React logo" />
-        </a>
+        <input type="text" name="Todo_Task" id="todo_enter" className='bg-slate-300 py-2 w-full rounded-md focus:outline-none px-2 mb-4'/>
+        <button id='add_todo' className='border-1 border-cyan-500' onClick={addTask}>Add Todo</button>
       </div>
-      <h1>Vite + React</h1>
-      <div >
-        <button onClick={changeCount}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='mt-12 bg-gray-300 h-full w-1/3 text-center py-3 rounded-lg'>
+        <div className='text-zinc-900 w-full h-full text-start'>
+        {todoList ? todoList : <p>No Task added yet!!!</p>}
+        </div>
       </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
